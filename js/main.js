aaedd074f31f0bb52272713d1eaf9709220126e1
Ross Vandenhoeck <rvandenhoeck@gmail.com>
2024-01-22 11:52:24 -0500

    init

---
+++ js/main.js
@@ -0,0 +1,53 @@
+// GLOBAL VARIABLES
+let pageWidth = innerWidth;
+let pageHeight = innerHeight;
+
+document.addEventListener('resize', () => {
+	pageWidth = innerWidth;
+	pageHeight = innerHeight;
+})
+
+const path = location.pathname.replace("/","");
+
+// LINK HANDLING
+const navLinks = document.querySelectorAll('header a');
+navLinks.forEach(link => {
+	if (link.href.includes(path)) link.classList.add('is-active');
+})
+
+
+// CUBE
+const cube = document.getElementById('cube');
+
+if (cube) {
+	// set defaults
+	const frameRate = 24;
+	const translateZ = pageWidth > 900 ? 200 : 75;
+	let mouseX = pageWidth / -4;
+	let mouseY = pageHeight / -4;
+	let rotateX = 40;
+	let rotateY = 40;
+
+	const spinCube = () => {
+		rotateY = rotateY + (mouseX / pageHeight * 180 - rotateY) / frameRate;
+		rotateX = rotateX + (mouseY / pageWidth * 360 - rotateX) / frameRate;
+
+		cube.style.transform = `translateZ(${translateZ}px) rotateX(${-rotateX}deg) rotateZ(${rotateY}deg)`;
+	};
+
+	// init
+	spinCube();
+
+	// update on frame rate
+	setInterval(() => {
+		spinCube();
+	}, 1000 / frameRate);
+
+
+	// update target coords
+	document.addEventListener('mousemove', e => {
+		mouseX = e.x;
+		mouseY = e.y;
+	});
+
+}
